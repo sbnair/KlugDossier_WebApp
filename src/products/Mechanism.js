aa6737a38155234ddfc54acda6e0ba6082406522
sbnair <shai567@live.com>
2021-04-01 16:05:10 +0530

    Check in the changes done in website

--- src/products/Mechanism.js
+++ src/products/Mechanism.js
@@ -5,7 +5,7 @@ export default () => (
   <div className="container">
     <h2>PolkaDossier AI</h2>
     
-    <p>PolkaDossier will have AI model integrated as shown below.</p>
+    <p>PolkaDossier will have AI engine which will built AI models based on the consumer requirements and also the prediction accuracy that consumer needs.</p>
     
     <div>
         <img src={require('./PolkaDossierAI.png')} alt="" />
